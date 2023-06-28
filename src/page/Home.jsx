cb0b3b1f42cf217b5856e340dce6ea7b87e1f4e1
Olevova <olevova1983@gmail.com>
2023-06-28 23:33:37 +0300

    nekk

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,5 @@
 import Header from "../components/Header/Header";
-import { Hero } from "../components/Hero/Hero";
+// import { Hero } from "../components/Hero/Hero";
 import { Footer } from "../components/Footer/Footer";
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
